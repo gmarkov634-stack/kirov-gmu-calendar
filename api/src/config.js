50c70e155b3520cff1c433cefce4de06ac272cba
gmarkov634-stack <gmarkov634@gmail.com>
2026-08-08 20:42:09 +0300

    Добавить api/src/config.js

---
+++ api/src/config.js
@@ -0,0 +1,15 @@
+import path from "node:path";
+
+export function loadConfig(env = process.env) {
+  return {
+    port: Number(env.PORT || 8080),
+    allowedOrigin: env.ALLOWED_ORIGIN || "https://gmarkov634-stack.github.io",
+    bucket: env.S3_BUCKET || "kgmu-calendar-data-gmarkov634",
+    endpoint: env.S3_ENDPOINT || "https://s3.cloud.ru",
+    region: env.S3_REGION || "ru-central-1",
+    accessKeyId: env.S3_ACCESS_KEY_ID || "",
+    secretAccessKey: env.S3_SECRET_ACCESS_KEY || "",
+    dataDir: env.DATA_DIR || path.resolve("data"),
+    cacheTtlMs: Number(env.CACHE_TTL_MS || 300000),
+  };
+}
