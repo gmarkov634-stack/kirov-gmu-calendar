b084cbb5b18a83829c20a7879faccc02ac82f704
gmarkov634-stack <gmarkov634@gmail.com>
2026-08-08 19:40:57 +0300

    Добавить data.js для первой версии сайта

---
+++ data.js
@@ -0,0 +1,8 @@
+window.CALENDAR_DATA = {
+  faculties: [
+    { id: "medicine", name: "Лечебный факультет", short: "Лечебное дело", icon: "Л", courses: 6, groups: {} },
+    { id: "pediatrics", name: "Педиатрический факультет", short: "Педиатрия", icon: "П", courses: 6, groups: { 1: ["131", "132", "133", "134", "135", "136", "137", "138", "139"] } },
+    { id: "dentistry", name: "Стоматологический факультет", short: "Стоматология", icon: "С", courses: 5, groups: {} },
+  ],
+  calendars: {},
+};
