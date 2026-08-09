1dcb296fd790349617c2126b5e9c8415bf3c8054
gmarkov634-stack <gmarkov634@gmail.com>
2026-08-10 00:15:36 +0300

    Configure production Cloud.ru API for ОмГМУ landing

---
+++ omgmu/config.js
@@ -0,0 +1,8 @@
+window.OMGMU_CONFIG = Object.freeze({
+  university: "omgmu",
+  program: "medicine-international",
+  timezone: "Asia/Omsk",
+  apiBaseUrl: "https://kgmu-calendar-api.containerapps.ru",
+  paymentPath: "/api/v2/payments",
+  priceRub: 490,
+});
