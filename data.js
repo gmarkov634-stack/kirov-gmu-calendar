578368485e06c0ab3b6698e6141121bd1508a12a
gmarkov634-stack <gmarkov634@gmail.com>
2026-08-09 16:34:53 +0300

    Declare checkout availability for КГМУ

--- data.js
+++ data.js
@@ -9,7 +9,7 @@ window.CALENDAR_DATA = {
     themeColor: "#1559d6",
   },
   apiBase: "https://kgmu-calendar-api.containerapps.ru",
-  offer: { price: "490 ₽", academicYear: "2025/26", semester: 2, expires: "31 августа 2026", testMode: true },
+  offer: { price: "490 ₽", academicYear: "2025/26", semester: 2, expires: "31 августа 2026", testMode: true, checkoutEnabled: true },
   faculties: [
     { id: "medicine", name: "Лечебный факультет", short: "Лечебное дело", icon: "Л", courses: 6, groups: {} },
     { id: "pediatrics", name: "Педиатрический факультет", short: "Педиатрия", icon: "П", courses: 6, groups: { 1: ["131", "132", "133", "134", "135", "136", "137", "138", "139"] } },
