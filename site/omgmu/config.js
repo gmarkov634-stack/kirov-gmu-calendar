3d658d3187f487d7dbfa315d9d0fb1097427f173
gmarkov634-stack <gmarkov634@gmail.com>
2026-08-09 23:14:22 +0300

    Add ОмГМУ landing configuration

---
+++ site/omgmu/config.js
@@ -0,0 +1,8 @@
+window.OMGМU_CONFIG = Object.freeze({
+  university: "omgmu",
+  program: "medicine-international",
+  timezone: "Asia/Omsk",
+  apiBaseUrl: "https://REPLACE_WITH_CLOUD_RU_API",
+  paymentPath: "/api/v2/payments",
+  priceRub: 490,
+});
