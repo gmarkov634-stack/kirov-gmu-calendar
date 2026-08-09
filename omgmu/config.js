9c8363159d31f331d5be0ebab6bec0fd3433871a
gmarkov634-stack <gmarkov634@gmail.com>
2026-08-10 00:43:41 +0300

    Temporarily pause ОмГМУ checkout during schedule QA

--- omgmu/config.js
+++ omgmu/config.js
@@ -2,7 +2,7 @@ window.OMGMU_CONFIG = Object.freeze({
   university: "omgmu",
   program: "medicine-international",
   timezone: "Asia/Omsk",
-  apiBaseUrl: "https://kgmu-calendar-api.containerapps.ru",
+  apiBaseUrl: "https://REPLACE_WITH_CLOUD_RU_API",
   paymentPath: "/api/v2/payments",
   priceRub: 490,
 });
