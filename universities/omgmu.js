e649ee67833066346a3288c946202624729079f0
gmarkov634-stack <gmarkov634@gmail.com>
2026-08-09 16:30:09 +0300

    Add initial OmSMU configuration

---
+++ universities/omgmu.js
@@ -0,0 +1,25 @@
+window.CALENDAR_DATA = {
+  university: {
+    id: "omgmu",
+    shortName: "ОмГМУ",
+    name: "Омский государственный медицинский университет",
+    city: "Омск",
+    timezone: "Asia/Omsk",
+    brandLetter: "О",
+    themeColor: "#1559d6",
+  },
+  apiBase: "https://kgmu-calendar-api.containerapps.ru",
+  offer: {
+    price: "490 ₽",
+    academicYear: "2026/27",
+    semester: 1,
+    expires: "31 августа 2027",
+    testMode: true,
+  },
+  faculties: [
+    { id: "medicine", name: "Лечебный факультет", short: "Лечебное дело", icon: "Л", courses: 6, groups: {} },
+    { id: "pediatrics", name: "Педиатрический факультет", short: "Педиатрия", icon: "П", courses: 6, groups: {} },
+    { id: "dentistry", name: "Стоматологический факультет", short: "Стоматология", icon: "С", courses: 5, groups: {} },
+    { id: "pharmacy", name: "Фармацевтический факультет", short: "Фармация", icon: "Ф", courses: 5, groups: {} },
+  ],
+};
