a9fc12cd13960cafe8198d02cf0d7a840f2206f2
gmarkov634-stack <gmarkov634@gmail.com>
2026-08-10 12:55:02 +0300

    Expose confirmed test checkout mode on ОмГМУ landing

--- site/omgmu/config.js
+++ site/omgmu/config.js
@@ -5,5 +5,6 @@ window.OMGMU_CONFIG = Object.freeze({
   apiBaseUrl: "https://REPLACE_WITH_CLOUD_RU_API",
   paymentPath: "/api/v2/payments",
   priceRub: 490,
+  testMode: true,
   checkoutEnabled: false,
 });
