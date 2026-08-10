2cd15240b82f343ea97fe769a9d318b494693a76
gmarkov634-stack <gmarkov634@gmail.com>
2026-08-10 19:07:55 +0300

    Propagate deploy version to mobile stylesheet

--- site/omgmu/config.js
+++ site/omgmu/config.js
@@ -10,8 +10,13 @@ window.OMGMU_CONFIG = Object.freeze({
 });
 
 if (typeof document !== "undefined" && document.currentScript) {
+  const scriptUrl = new URL(document.currentScript.src);
+  const mobileUrl = new URL("mobile.css", scriptUrl);
+  const version = scriptUrl.searchParams.get("v");
+  if (version) mobileUrl.searchParams.set("v", version);
+
   const omgmuMobileStyles = document.createElement("link");
   omgmuMobileStyles.rel = "stylesheet";
-  omgmuMobileStyles.href = new URL("mobile.css", document.currentScript.src).href;
+  omgmuMobileStyles.href = mobileUrl.href;
   document.head.append(omgmuMobileStyles);
 }
