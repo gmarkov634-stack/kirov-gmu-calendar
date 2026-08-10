d6dcb17d66b10a91efccedfc15da58b5e4b6990f
gmarkov634-stack <gmarkov634@gmail.com>
2026-08-10 15:49:28 +0300

    Enable ОмГМУ test checkout source config

--- site/omgmu/config.js
+++ site/omgmu/config.js
@@ -2,9 +2,9 @@ window.OMGMU_CONFIG = Object.freeze({
   university: "omgmu",
   program: "medicine-international",
   timezone: "Asia/Omsk",
-  apiBaseUrl: "https://REPLACE_WITH_CLOUD_RU_API",
+  apiBaseUrl: "https://kgmu-calendar-api.containerapps.ru",
   paymentPath: "/api/v2/payments",
   priceRub: 490,
   testMode: true,
-  checkoutEnabled: false,
+  checkoutEnabled: true,
 });
