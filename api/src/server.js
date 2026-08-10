607f970e8f8c088bb6329479b1e2ef4e0fb8c016
gmarkov634-stack <gmarkov634@gmail.com>
2026-08-10 21:22:54 +0300

    Подключить endpoint чтения стены VK

--- api/src/server.js
+++ api/src/server.js
@@ -3,6 +3,7 @@ import { createHandler } from "./app.js";
 import { loadConfig } from "./config.js";
 import { MultiUniversityStore } from "./university-store.js";
 import { createVkCallbackHandler } from "./vk-callback.js";
+import { createVkWallHandler } from "./vk-wall.js";
 import { YooKassaService } from "./yookassa.js";
 
 const config = loadConfig();
@@ -10,11 +11,15 @@ const store = new MultiUniversityStore(config);
 const payments = new YooKassaService({ store, config });
 const appHandler = createHandler({ store, config, payments });
 const vkCallbackHandler = createVkCallbackHandler(process.env, { store });
+const vkWallHandler = createVkWallHandler();
 
 const server = http.createServer((request, response) => {
   const url = new URL(request.url, "http://localhost");
   if (request.method === "POST" && url.pathname === "/api/v1/vk/callback") {
     return vkCallbackHandler(request, response);
+  }
+  if (url.pathname === "/api/v1/vk/wall") {
+    return vkWallHandler(request, response);
   }
   return appHandler(request, response);
 });
