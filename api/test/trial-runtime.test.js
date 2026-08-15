ad25f96fc4f1e089885bef6bb9802a1f5b9a5959
gmarkov634-stack <gmarkov634@gmail.com>
2026-08-15 12:11:18 +0300

    test: cover trial runtime handlers

---
+++ api/test/trial-runtime.test.js
@@ -0,0 +1,7 @@
+import assert from "node:assert/strict";
+import fs from "node:fs/promises";
+import http from "node:http";
+import os from "node:os";
+import path from "node:path";
+import test from "node:test";
+import { TrialHttpHandlerPlaceholder } from "../src/trial-runtime-test-placeholder.js";
