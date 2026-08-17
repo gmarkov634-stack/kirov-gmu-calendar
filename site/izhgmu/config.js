ab643b0e977f7ecd671e47b924308f6a74003d1c
gmarkov634-stack <gmarkov634@gmail.com>
2026-08-17 04:04:42 +0300

    frontend: bind IzhGMU prelaunch to shared API

--- site/izhgmu/config.js
+++ site/izhgmu/config.js
@@ -1,6 +1,9 @@
 window.IZHGMU_CONFIG = Object.freeze({
-  apiBaseUrl: "https://api.calendars.you/",
+  apiBaseUrl: "https://kgmu-calendar-api.containerapps.ru",
   university: "izhgmu",
   program: "medicine",
+  timezone: "Europe/Samara",
+  paymentPath: "/api/v2/payments",
+  defaultPlan: "semester",
   prelaunchCourses: Object.freeze([1, 2, 3]),
 });
