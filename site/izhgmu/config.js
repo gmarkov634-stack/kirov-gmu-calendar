fea3a18370c7ad4e8d7af42923ed8a0eb87c4fc0
gmarkov634-stack <gmarkov634@gmail.com>
2026-08-17 03:36:46 +0300

    feat: add IzhGMU prelaunch catalog config

---
+++ site/izhgmu/config.js
@@ -0,0 +1,6 @@
+window.IZHGMU_CONFIG = Object.freeze({
+  apiBaseUrl: "https://api.calendars.you/",
+  university: "izhgmu",
+  program: "medicine",
+  prelaunchCourses: Object.freeze([1, 2, 3]),
+});
