5f96acadab8d596c242891753ee44bf4243f2c5a
gmarkov634-stack <gmarkov634@gmail.com>
2026-08-17 04:16:25 +0300

    omgmu: make landing config non-commercial

--- site/omgmu/config.js
+++ site/omgmu/config.js
@@ -1,17 +1,20 @@
 window.OMGMU_CONFIG = Object.freeze({
   university: "omgmu",
-  program: "medicine-international",
-  timezone: "Asia/Omsk",
   apiBaseUrl: "https://kgmu-calendar-api.containerapps.ru",
   paymentPath: "/api/v2/payments",
-  priceRub: 490,
-  testMode: true,
-  checkoutEnabled: true,
+  programLabels: Object.freeze({
+    medicine: Object.freeze({ title: "Лечебное дело", code: "31.05.01" }),
+    "medicine-international": Object.freeze({ title: "Лечебное дело", subtitle: "для иностранных обучающихся" }),
+    pediatrics: Object.freeze({ title: "Педиатрия", code: "31.05.02" }),
+    dentistry: Object.freeze({ title: "Стоматология", code: "31.05.03" }),
+    "preventive-medicine": Object.freeze({ title: "Медико-профилактическое дело", code: "32.05.01" }),
+    pharmacy: Object.freeze({ title: "Фармация", code: "33.05.01" }),
+  }),
 });
 
 if (typeof document !== "undefined" && document.currentScript) {
   const omgmuMobileStyles = document.createElement("link");
   omgmuMobileStyles.rel = "stylesheet";
   omgmuMobileStyles.href = new URL("mobile.css", document.currentScript.src).href;
-  document.head.append(omgmuMobileStyles);
+  document.head.appendChild(omgmuMobileStyles);
 }
