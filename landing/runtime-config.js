d9b9041ba234d1fb1c19397bd5edd16312a4cfd1
gmarkov634-stack <gmarkov634@gmail.com>
2026-08-28 09:14:50 +0300

    Add fail-closed landing runtime config

---
+++ landing/runtime-config.js
@@ -0,0 +1,11 @@
+window.MEDCAL_KGMU_CONFIG = Object.freeze({
+  universityId: 'kirov-gmu',
+  academicYearId: '2026-2027',
+  academicPeriodId: '2026-2027-semester-1',
+  trialEnabled: false,
+  managementEnabled: false,
+  commerceEnabled: false,
+  publishedGroupIds: [],
+  vkUrl: '',
+  supportUrl: ''
+});
