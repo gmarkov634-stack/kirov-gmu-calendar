874b678c7c6cee5b98b398dca214149c82875ee3
gmarkov634-stack <gmarkov634@gmail.com>
2026-08-31 16:59:14 +0300

    Load trial personalization in Pages artifact

--- deploy/build-pages.sh
+++ deploy/build-pages.sh
@@ -20,6 +20,8 @@ cp "$ROOT_DIR/deploy/runtime-config.pages.js" "$OUT_DIR/runtime-config.js"
 cp "$ROOT_DIR/catalog/2026-2027-semester-1.json" "$OUT_DIR/catalog/2026-2027-semester-1.json"
 sed -i '/<script type="module" src="\.\/app\.js"><\/script>/i\    <script src="./acquisition-ui.js"></script>' "$OUT_DIR/index.html"
 sed -i '/<script type="module" src="\.\/app\.js"><\/script>/i\    <script src="./acquisition-ux-refinements.js"></script>' "$OUT_DIR/index.html"
+sed -i '/<script type="module" src="\.\/app\.js"><\/script>/i\    <script src="./trial-personalization.js"></script>' "$OUT_DIR/index.html"
+sed -i '/<script type="module" src="\.\/manage\.js"><\/script>/i\  <script src="./elective-empty-state.js"></script>' "$OUT_DIR/manage/index.html"
 sed -i '/<\/body>/i\    <script src="./availability-status.js"></script>' "$OUT_DIR/index.html"
 : > "$OUT_DIR/.nojekyll"
 
